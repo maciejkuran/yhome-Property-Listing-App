d22621dcd63ced186a035e03ed432f66adec9a3f
Maciej Kuran - Janowski <maciejkuran@gmail.com>
2022-08-01 00:40:14 +0200

    commit

---
+++ js/main.js
@@ -0,0 +1,142 @@
+'use strict';
+
+import { Property } from './properties_data.js';
+import { allProperties } from './properties_data.js';
+
+/////DISPLAY DYNAMICALLY ALL PROPERTIES
+const propertiesContainer = document.querySelector('.properties-container');
+
+const displayProperties = () => {
+  allProperties.forEach(property => {
+    const aTag = document.createElement('a');
+    aTag.classList.add('open-property-page-btn');
+    aTag.href = '#';
+    aTag.innerHTML = `<div class="property">
+  <picture class="property-img">
+    <img class="img-label" src="${property.listingImg}" alt="" />
+    <p class="status-label">${property.status}</p>
+  </picture>
+  <div class="price">
+    <p class="price-label"><i class="ri-price-tag-3-line"></i>${property.displayPrice()}</p>
+  </div>
+  <div class="property-data">
+    <ul>
+      <li class="country-label">
+        <i class="ri-map-pin-2-line"></i>${
+          property.country
+        },<span class="city-label"> ${property.city}</span>
+      </li>
+      <li class="type-label"><i class="ri-home-line"></i>${property.type}</li>
+      <li class="living-area-label"><i class="ri-bookmark-line"></i>${property.displayLivingArea()}²</li>
+    </ul>
+  </div>
+  <div class="action-buttons">
+    <button class="contact-agent-btn"><i class="ri-contacts-line"></i></button>
+    <button class="add-to-favorites-btn"><i class="ri-heart-3-line"></i></button>
+  </div>
+</div>`;
+    propertiesContainer.append(aTag);
+  });
+};
+
+displayProperties();
+
+/////FILTERING RESULTS
+
+//Retreiving unique property values and saving in arrays
+const uniqueValues = objectKey => {
+  const arr = [];
+  for (const property of allProperties) {
+    arr.push(property[`${objectKey}`]);
+  }
+  return [...new Set(arr)];
+};
+
+const countriesArr = uniqueValues('country');
+const citiesArr = uniqueValues('city');
+const typesArr = uniqueValues('type');
+const statusesArr = uniqueValues('status');
+
+//Filling filter options with the unique values
+const createOptions = (wrapperClass, arr, elClass) => {
+  const wrapper = document.querySelector(`.options-${wrapperClass}`);
+
+  arr.forEach(el => {
+    const btn = document.createElement('button');
+    btn.className = `${elClass}-option option`;
+    btn.textContent = el;
+    wrapper.append(btn);
+  });
+};
+
+//Creating all options in DOM
+const initOptions = () => {
+  createOptions('countries', countriesArr, 'country');
+  createOptions('cities', citiesArr, 'city');
+  createOptions('types', typesArr, 'type');
+  createOptions('statuses', statusesArr, 'status');
+};
+
+initOptions();
+
+//Toggling options container on input click
+const formsWrapper = document.querySelector('.forms-wrapper');
+const transparentOverlay = document.querySelector(
+  '.transparent-background-overlay'
+);
+
+formsWrapper.addEventListener('click', e => {
+  removeActiveClass();
+  let target;
+
+  if (!e.target.classList.contains('input')) return false;
+
+  if (e.target.classList.contains('input')) {
+    target = e.target.getAttribute('name');
+    const optionsContainer = document.querySelector(
+      `.options-container[name="${target}"]`
+    );
+
+    optionsContainer.classList.toggle('options-container-active');
+  }
+});
+
+//Removing class active from all elements
+const optionsContainer = document.querySelectorAll('.options-container');
+
+const removeActiveClass = () => {
+  optionsContainer.forEach(container => {
+    container.classList.remove('options-container-active');
+  });
+};
+
+/////////////////////////DONE IN THE TESTING STAGE
+// Filter bar - testing logic
+// const input = document.querySelector(".input");
+// const options = document.querySelectorAll(".option");
+// const optionsContainer = document.querySelector(".options-container");
+// const countryLabels = document.querySelectorAll(".country-label");
+// const countryLabelsWrapper = document.querySelector(".country-labels-wrapper");
+
+// optionsContainer.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   if (e.target.classList.contains("option")) {
+//     let target = e.target;
+//     input.value = target.textContent;
+//     console.log(input.value);
+//     filterResults(countryLabels);
+//   }
+
+//   if (!e.target.classList.contains("option")) return false;
+// });
+
+// //filtering
+// const filterResults = (elements, input) => {
+//   elements.forEach((el) => {
+//     if (el.textContent.indexOf(input.value) !== -1) {
+//       el.style.display = "inline";
+//     } else {
+//       el.style.display = "none";
+//     }
+//   });
+// };
---
+++ js/properties_data.js
@@ -0,0 +1,217 @@
+//////Properties DATA
+export const allProperties = [];
+
+export const Property = class {
+  constructor(status, type, country, city, livingArea, price, listingImg) {
+    this.status = status;
+    this.type = type;
+    this.country = country;
+    this.city = city;
+    this.livingArea = livingArea;
+    this.price = price;
+    this.listingImg = listingImg;
+    allProperties.push(this);
+  }
+
+  displayPrice() {
+    const timeZone = JSON.stringify(
+      new Intl.DateTimeFormat().resolvedOptions()
+    );
+    const locale = navigator.language;
+    const dollarOptions = { style: 'currency', currency: 'USD' };
+    const euroOptions = { style: 'currency', currency: 'EUR' };
+    const eurToUSD = this.price * 1.02; //rate conversion 31.07.2022;
+    //If user is from Europe = EUR, if not = display $;
+    if (timeZone.includes('Europe')) {
+      return new Intl.NumberFormat(locale, euroOptions).format(this.price);
+    } else {
+      return new Intl.NumberFormat(locale, dollarOptions).format(eurToUSD);
+    }
+  }
+
+  displayLivingArea() {
+    const options = { style: 'unit', unit: 'meter' };
+    const locale = navigator.language;
+
+    return new Intl.NumberFormat(locale, options).format(this.livingArea);
+  }
+};
+
+////Portugal - Property
+const property_portugal_1 = new Property(
+  'For Sale',
+  'Condo',
+  'Portugal',
+  'Lisbon',
+  85,
+  230000,
+  '/img/property-portugal-1.jpg'
+);
+const property_portugal_2 = new Property(
+  'For Rent',
+  'Condo',
+  'Portugal',
+  'Lisbon',
+  52,
+  1600,
+  '/img/property-portugal-2.jpg'
+);
+const property_portugal_3 = new Property(
+  'For Sale',
+  'House',
+  'Portugal',
+  'Porto',
+  120,
+  350000,
+  '/img/property-portugal-3.jpg'
+);
+const property_portugal_4 = new Property(
+  'For Sale',
+  'Condo',
+  'Portugal',
+  'Porto',
+  95,
+  280000,
+  '/img/property-portugal-4.jpg'
+);
+const property_portugal_5 = new Property(
+  'For Sale',
+  'House',
+  'Portugal',
+  'Porto',
+  180,
+  900000,
+  '/img/property-portugal-5.jpg'
+);
+
+////Portugal - Property
+const property_spain_1 = new Property(
+  'For Rent',
+  'Condo',
+  'Spain',
+  'Malaga',
+  85,
+  2000,
+  '/img/property-spain-1.jpg'
+);
+const property_spain_2 = new Property(
+  'For Sale',
+  'Condo',
+  'Spain',
+  'Malaga',
+  45,
+  180000,
+  '/img/property-spain-2.jpg'
+);
+const property_spain_3 = new Property(
+  'For Sale',
+  'House',
+  'Spain',
+  'Malaga',
+  130,
+  350000,
+  '/img/property-spain-3.jpg'
+);
+const property_spain_4 = new Property(
+  'For Sale',
+  'House',
+  'Spain',
+  'Barcelona',
+  110,
+  520000,
+  '/img/property-spain-4.jpg'
+);
+const property_spain_5 = new Property(
+  'For Sale',
+  'House',
+  'Spain',
+  'Barcelona',
+  150,
+  600000,
+  '/img/property-spain-5.jpg'
+);
+const property_spain_6 = new Property(
+  'For Sale',
+  'House',
+  'Spain',
+  'Barcelona',
+  320,
+  1800000,
+  '/img/property-spain-6.jpg'
+);
+
+////Italy - Property
+const property_italy_1 = new Property(
+  'For Sale',
+  'House',
+  'Italy',
+  'Bologna',
+  190,
+  650000,
+  '/img/property-italy-1.jpg'
+);
+const property_italy_2 = new Property(
+  'For Rent',
+  'Condo',
+  'Italy',
+  'Bologna',
+  80,
+  1900,
+  '/img/property-italy-2.jpg'
+);
+const property_italy_3 = new Property(
+  'For Sale',
+  'Condo',
+  'Italy',
+  'Rome',
+  60,
+  290000,
+  '/img/property-italy-3.jpg'
+);
+const property_italy_4 = new Property(
+  'For Sale',
+  'House',
+  'Italy',
+  'Rome',
+  130,
+  800000,
+  '/img/property-italy-4.jpg'
+);
+
+////Poland - Property
+const property_poland_1 = new Property(
+  'For Sale',
+  'Condo',
+  'Poland',
+  'Warsaw',
+  70,
+  220000,
+  '/img/property-poland-1.jpg'
+);
+const property_poland_2 = new Property(
+  'For Sale',
+  'Condo',
+  'Poland',
+  'Warsaw',
+  90,
+  280000,
+  '/img/property-poland-2.jpg'
+);
+const property_poland_3 = new Property(
+  'For Sale',
+  'House',
+  'Poland',
+  'Warsaw',
+  140,
+  450000,
+  '/img/property-poland-3.jpg'
+);
+const property_poland_4 = new Property(
+  'For Rent',
+  'Condo',
+  'Poland',
+  'Warsaw',
+  30,
+  900,
+  '/img/property-poland-4.jpg'
+);
